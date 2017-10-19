9e245f912914cd4eba6b0fd505794cd4a2634ff7
Volodymyr Shamray <shamray@outlook.com>
2017-10-19 12:09:47 +0300

    Added signature calculation tests

--- src/main.cpp
+++ src/main.cpp
@@ -10,35 +10,67 @@ struct simple : Test {
     dstu4145::ecurve curve{
         field,
         1,
-        dstu4145::integer{"0x5FF6108462A2DC8210AB403925E638A19C1455D21" },
-        dstu4145::integer{"0x400000000000000000002BEC12BE2262D39BCF14D" }
+        dstu4145::integer{"0x5FF6108462A2DC8210AB403925E638A19C1455D21"},
+        dstu4145::integer{"0x400000000000000000002BEC12BE2262D39BCF14D"}
     };
 
     dstu4145::ecurve::point p{
         curve,
-        dstu4145::integer{"0x72D867F93A93AC27DF9FF01AFFE74885C8C540420" },
-        dstu4145::integer{"0x0224A9C3947852B97C5599D5F4AB81122ADC3FD9B" }
+        dstu4145::integer{"0x72D867F93A93AC27DF9FF01AFFE74885C8C540420"},
+        dstu4145::integer{"0x0224A9C3947852B97C5599D5F4AB81122ADC3FD9B"}
     };
 
-    dstu4145::integer d{"0x183F60FDF7951FF47D67193F8D073790C1C9B5A3E" };
+    dstu4145::integer d{"0x183F60FDF7951FF47D67193F8D073790C1C9B5A3E"};
+    dstu4145::integer n{"0x400000000000000000002BEC12BE2262D39BCF14D"};
 };
 
 TEST_F(simple, public_key_computation) {
     auto expected = dstu4145::ecurve::point{
         curve,
-        dstu4145::integer{"0x57DE7FDE023FF929CB6AC785CE4B79CF64ABDC2DA" },
-        dstu4145::integer{"0x3E85444324BCF06AD85ABF6AD7B5F34770532B9AA" }
+        dstu4145::integer{"0x57DE7FDE023FF929CB6AC785CE4B79CF64ABDC2DA"},
+        dstu4145::integer{"0x3E85444324BCF06AD85ABF6AD7B5F34770532B9AA"}
     };
     auto q = -(d * p);
 
     EXPECT_EQ(q, expected);
 }
 
+TEST_F(simple, presignature_calculation) {
+    auto expected = dstu4145::ecurve::point{
+        curve,
+        dstu4145::integer{"0x42A7D756D70E1C9BA62D2CB43707C35204EF3C67C"},
+        dstu4145::integer{"0x5310AE5E560464A95DC80286F17EB762EC544B15B"}
+    };
+
+    auto e = dstu4145::integer{"0x1025E40BD97DB012B7A1D79DE8E12932D247F61C6"};
+    auto ep = e * p;
+    EXPECT_EQ(ep, expected);
+
+    auto fe = static_cast<dstu4145::integer>(ep.x);
+    EXPECT_EQ(fe, dstu4145::integer{ "0x42A7D756D70E1C9BA62D2CB43707C35204EF3C67C" });
+}
+
+TEST_F(simple, signature_calculation) {
+    auto hash = ::dstu4145::integer{ "0x09C9C44277910C9AAEE486883A2EB95B7180166DDF73532EEB76EDAEF52247FF" };
+
+    auto e = dstu4145::integer{ "0x1025E40BD97DB012B7A1D79DE8E12932D247F61C6" };
+    auto fe = (e * p).x;
+
+    auto h = field.create_element(hash);
+    auto r = static_cast<dstu4145::integer>(h * fe);
+
+    auto dr = static_cast<dstu4145::integer>((static_cast<dstu4145::integer2>(d) * r) % n);
+    auto s = (e + dr) % n;
+
+    EXPECT_EQ(r, dstu4145::integer{"0x274EA2C0CAA014A0D80A424F59ADE7A93068D08A7"});
+    EXPECT_EQ(s, dstu4145::integer{"0x2100D86957331832B8E8C230F5BD6A332B3615ACA"});
+}
+
 TEST_F(simple, point_multiplication) {
     auto expected = dstu4145::ecurve::point{
         curve,
-        dstu4145::integer{"0x57DE7FDE023FF929CB6AC785CE4B79CF64ABDC2DA" },
-        dstu4145::integer{"0x695B3B9D26830943133078EF19FE8A8814F8F7B70" }
+        dstu4145::integer{"0x57DE7FDE023FF929CB6AC785CE4B79CF64ABDC2DA"},
+        dstu4145::integer{"0x695B3B9D26830943133078EF19FE8A8814F8F7B70"}
     };
     auto q = d * p;
     EXPECT_EQ(q, expected);
@@ -47,8 +79,8 @@ TEST_F(simple, point_multiplication) {
 TEST_F(simple, point_addition_equal) {
     auto expected = dstu4145::ecurve::point{
         curve,
-        dstu4145::integer{"0x5A4397672F0C513E390212AD3825420BE83E3BFDA" },
-        dstu4145::integer{"0x378C6CADAC80077C50EC218AB8C96015750C83564" }
+        dstu4145::integer{"0x5A4397672F0C513E390212AD3825420BE83E3BFDA"},
+        dstu4145::integer{"0x378C6CADAC80077C50EC218AB8C96015750C83564"}
     };
     auto q = p + p;
     EXPECT_EQ(q, expected);
@@ -57,14 +89,14 @@ TEST_F(simple, point_addition_equal) {
 TEST_F(simple, point_addition_not_equal) {
     auto expected = dstu4145::ecurve::point{
         curve,
-        dstu4145::integer{"0x61DA3389DDA324607BFFCC1EAD469465D033EDE07" },
-        dstu4145::integer{"0x142B39F71B08662634682D26D3A1F4BE543B393E9" }
+        dstu4145::integer{"0x61DA3389DDA324607BFFCC1EAD469465D033EDE07"},
+        dstu4145::integer{"0x142B39F71B08662634682D26D3A1F4BE543B393E9"}
     };
 
     auto r = dstu4145::ecurve::point{
         curve,
-        dstu4145::integer{"0x5A4397672F0C513E390212AD3825420BE83E3BFDA" },
-        dstu4145::integer{"0x378C6CADAC80077C50EC218AB8C96015750C83564" }
+        dstu4145::integer{"0x5A4397672F0C513E390212AD3825420BE83E3BFDA"},
+        dstu4145::integer{"0x378C6CADAC80077C50EC218AB8C96015750C83564"}
     };
     auto q = r + p;
     EXPECT_EQ(q, expected);

a2bb86c6f9433f28e8a7686bc847bf7527b72a97
Volodymyr Shamray <shamray@outlook.com>
2018-04-30 23:28:26 +0300

    Fix public key constructor

--- src/key_pair.cpp
+++ src/key_pair.cpp
@@ -9,7 +9,7 @@ namespace dstu4145
     }
 
     public_key::public_key(const domain_params& params, const dstu4145::private_key& value)
-        : q_(params.p * value)
+        : q_{-(params.p * value)}
     {
     }
 }

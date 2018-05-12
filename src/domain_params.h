737633526ee2cce6385297e7c99c9259a0cbdaf7
Volodymyr Shamray <shamray@outlook.com>
2018-05-12 10:01:49 +0300

    Add domain_params constructor with random base point

--- src/domain_params.h
+++ src/domain_params.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "ecurve.h"
+#include "integer.h"
+#include "rng_interface.h"
 
 namespace dstu4145
 {
@@ -9,5 +11,31 @@ namespace dstu4145
         ecurve curve;
         integer n;
         ecurve::point p;
+
+        domain_params(ecurve curve, integer n, ecurve::point p)
+            : curve{(curve)}
+            , n{n}
+            , p{p}
+        {}
+
+        domain_params(ecurve curve, integer n, rng_t& rng)
+            : curve{std::move(curve)}
+            , n{std::move(n)}
+            , p{this->curve.find_point(gen_random_integer(rng))}
+        {
+        }
+
+    private:
+        auto gen_random_integer(rng_t& rng) -> integer
+        {
+            auto buffer = std::vector<std::byte>{};
+
+            while(buffer.size() < 32) {
+                buffer.push_back(rng());
+            }
+
+            return buffer_to_integer(buffer);
+        }
+
     };
 }

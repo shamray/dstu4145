0c70bce9baa9fb1a110d3784cbe17e9561ab2f7c
Volodymyr Shamray <shamray@outlook.com>
2018-06-12 21:48:00 +0300

    Clean up code

--- src/ecurve.cpp
+++ src/ecurve.cpp
@@ -9,21 +9,11 @@ namespace dstu4145
             assert(u != gf.create_element(0));
             assert(w != gf.create_element(0));
 
-            //auto v = w * square(u.inverse());
-
-            auto u1 = u.inverse();
-            auto u2 = u1 * u1;
-            auto v = w * u2;
-
-            std::cout << std::endl << v << std::endl;
-            std::cout << std::endl << v.trace() << std::endl;
-            std::cout << std::endl << v.half_trace() << std::endl;
+            auto v = w * square(u.inverse());
 
             assert(v.trace() != gf.create_element(1));
 
-            std::cout << std::endl << std::hex << gf.basis() << std::endl;
             return v.half_trace() * u;
-//            return gf.create_element(dstu4145::integer{"0x0224A9C3947852B97C5599D5F4AB81122ADC3FD9B"});
         }
     }
     auto ecurve::find_point(integer ix, integer iy) const -> point
@@ -36,7 +26,6 @@ namespace dstu4145
         for (;;) {
             auto u = field().create_element(ix);
             auto w = u * u * u + field().create_element(a()) * u * u + field().create_element(b());
-            //std::cout << std::endl << w << std::endl;
             auto z = solve_quadratic_equasion(field(), u, w);
             if (z.has_value()) {
                 //assert(u * u * u + field().create_element(a()) * u * u + field().create_element(b()) == z.value() * z.value() + z.value() * u);

56c5a32115002d4d703b3ffbd24f8b27bc41e3e6
Volodymyr Shamray <shamray@outlook.com>
2018-08-06 21:25:00 +0300

    Add new files

---
+++ src/polynomial.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vshamray on 6.08.18.
+//
+
+#include "polynomial.h"
---
+++ src/polynomial.h
@@ -0,0 +1,16 @@
+//
+// Created by vshamray on 16.10.18.
+//
+
+#ifndef DSTU4145_POLYNOMIAL_H
+#define DSTU4145_POLYNOMIAL_H
+
+
+
+class polynomial {
+
+};
+
+
+
+#endif //DSTU4145_POLYNOMIAL_H

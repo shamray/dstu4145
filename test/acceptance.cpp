b0aad26752fd9373056795e8fcc72fa0134ddb09
Volodymyr Shamray <shamray@outlook.com>
2018-12-07 19:39:46 +0200

    Add acceptance test for 257 bit curve

--- test/acceptance.cpp
+++ test/acceptance.cpp
@@ -39,12 +39,11 @@ TEST_F(acceptance163, sign_and_verify)
 {
     auto fails = 0;
     for (auto i = 0; i < 20; ++i) {
-        auto base_point = curve.find_point(rng, n);
 
-        dstu4145::domain_params params {
+        dstu4145::domain_params params{
             curve,
             n,
-            base_point
+            rng
         };
 
         dstu4145::public_key pub_key{params, prv_key};
@@ -94,12 +93,10 @@ TEST_F(acceptance167, sign_and_verify)
 {
     auto fails = 0;
     for (auto i = 0; i < 20; ++i) {
-        auto base_point = curve.find_point(rng, n);
-
         dstu4145::domain_params params {
             curve,
             n,
-            base_point
+            rng
         };
 
         dstu4145::public_key pub_key{params, prv_key};
@@ -159,12 +156,10 @@ TEST_F(acceptance233, sign_and_verify)
 {
     auto fails = 0;
     for (auto i = 0; i < 20; ++i) {
-        auto base_point = curve.find_point(rng, n);
-
         dstu4145::domain_params params {
             curve,
             n,
-            base_point
+            rng
         };
 
         dstu4145::public_key pub_key{params, prv_key};
@@ -174,6 +169,59 @@ TEST_F(acceptance233, sign_and_verify)
         auto v = dstu4145::verifier{pub_key, params};
 
         auto signature = s.sign_hash(h);
+
+        if (!v.verify_hash(h, signature)) {
+            ++fails;
+
+            ADD_FAILURE() << std::hex
+                          << "p.x= " << params.p.x << std::endl
+                          << "p.y= " << params.p.y;
+        }
+    }
+
+    EXPECT_EQ(0, fails);
+}
+
+struct acceptance257 : Test
+{
+    dstu4145::ecurve curve{
+        dstu4145::gf2m{ 257, 12 },
+        0,
+        dstu4145::integer{ "1CEF494720115657E18F938D7A7942394FF9425C1458C57861F9EEA6ADBE3BE10" }
+    };
+    dstu4145::integer n{ "800000000000000000000000000000006759213AF182E987D3E17714907D470D" };
+
+    dstu4145::rng_t rng{
+        []() {
+            static std::random_device rd;
+            static std::mt19937 gen(rd());
+            std::uniform_int_distribution<unsigned char> dis(0);
+
+            return std::byte{ dis(gen) };
+        }
+    };
+
+    dstu4145::private_key prv_key{hex_buffer("00000000000000000000000183F60FDF7951FF47D67193F8D073790C1C9B5A3E"s)};
+};
+
+TEST_F(acceptance257, sign_and_verify)
+{
+    auto fails = 0;
+    for (auto i = 0; i < 10; ++i) {
+
+        dstu4145::domain_params params{
+            curve,
+            n,
+            rng
+        };
+
+        dstu4145::public_key pub_key{ params, prv_key };
+
+        auto h = hex_buffer("09C9C44277910C9AAEE486883A2EB95B7180166DDF73532EEB76EDAEF52247FF");
+        auto s = dstu4145::signer{ prv_key, params, rng };
+        auto v = dstu4145::verifier{ pub_key, params };
+
+        auto signature = s.sign_hash(h, 1024);
 
         if (!v.verify_hash(h, signature)) {
             ++fails;
@@ -240,3 +288,4 @@ TEST_F(acceptance509, sign_and_verify)
 
     EXPECT_EQ(0, fails);
 }
+

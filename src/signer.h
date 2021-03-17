c297984f38c221b913f7500d6805fe50d91cf093
Volodymyr Shamray <shamray@outlook.com>
2021-03-17 11:14:29 +0200

    Remove interface draft

--- src/signer.h
+++ src/signer.h
@@ -23,14 +23,4 @@ namespace dstu4145
     private:
 		domain_params params_;
     };
-
-    /*
-    struct presignature{};
-    using buffer = std::vector<std::byte>;
-
-    auto gen_private_key(domain_params, rng_t) -> private_key;
-    auto compute_presignature(domain_params, rng_t) -> presignature;
-    auto sign_hash(domain_params, private_key, presignature, buffer) -> buffer;
-	auto verify_hash(domain_params, public_key, buffer hash, buffer signature) -> bool;
-    */
 }
